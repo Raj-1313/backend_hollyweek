d216223a53e03b83cafbf3c0f253e3a73f099fed
subhamukherjee321 <subhamukherjee321@gmail.com>
2023-01-22 14:22:18 +0530

    Login Updated

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
-const express = require('express');
-const cors= require('cors');
-const connect= require('./config/config');
+const express = require("express");
+const cors = require("cors");
+const connect = require("./config/config");
 // Routes
-const  productRoute = require('./Route/ProductRoute');
+const productRoute = require("./Route/ProductRoute");
 const AuthRoute = require("./Route/Authantication_Route");
 const CartRoute = require("./Route/CartRoute");
 const AdminRoute = require("./Route/AdminRoutes");
@@ -13,47 +13,36 @@ const WishlistRoute = require("./Route/Wishliat_Route");
 const MostVisitedProduct_Route = require("./Route/MostVisitedProduct_Route");
 
 // middleware
-const AuthMiddleware = require('./middleware/Auth_Middleware');
+const AuthMiddleware = require("./middleware/Auth_Middleware");
 const mostVisitedProduct = require("./Route/MostVisitedProduct_Route");
 const app = express();
-
 
 app.use(express.json());
 app.use(cors());
 // All
-app.use("/auth",AuthRoute)
-app.use("/product",productRoute)
+app.use("/auth", AuthRoute);
+app.use("/product", productRoute);
 
-app.use(AuthMiddleware)
+app.use(AuthMiddleware);
 // Admin
-app.use("/admin",AdminRoute)
-app.use("/admin/mostvisted",mostVisitedProduct)
-app.use("/review",ReviewRoute)
-
+app.use("/admin", AdminRoute);
+app.use("/admin/mostvisted", mostVisitedProduct);
+app.use("/review", ReviewRoute);
 
 // users
-app.use("/cart",CartRoute)
-app.use("/wishlist",WishlistRoute)
+app.use("/cart", CartRoute);
+app.use("/wishlist", WishlistRoute);
 
-
-
-
-
-
-
-app.get('/',(req,res) => {
-res.send("I an onn")
-})
-
-
-
+app.get("/", (req, res) => {
+  res.send("I an onn");
+});
 
 app.listen(process.env.PORT, async () => {
-    
-    try{
-        await connect
-    console.log(`listning on port ${process.env.PORT}`)
-    }catch(e){
-        console.log(e.message)
-    }
-})
+  try {
+    await connect;
+    console.log("DB IS CONNECTED");
+  } catch (e) {
+    console.log(e.message);
+  }
+  console.log(`listning on port ${process.env.PORT}`);
+});
